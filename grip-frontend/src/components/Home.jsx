668fd86f7296755e0d198f8c6ca410de5f07f9e2
Harsh <harsh.mauny@gmail.com>
2021-03-27 18:54:36 +0530

    commit #9 image optimized

--- grip-frontend/src/components/Home.jsx
+++ grip-frontend/src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './Home.css'
 import { Link } from "react-router-dom";
-import '../assets/qr-code.png';
 import CountUp from "react-countup";
 import VisibilitySensor from 'react-visibility-sensor';
 
